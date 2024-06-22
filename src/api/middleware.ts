14f435c05a5e999b62b4d7c82ddac1f3bb1e806f
Cass Barnard <30302915+cass-codes@users.noreply.github.com>
2024-06-21 21:11:30 -0400

    updating log middleware

--- src/api/middleware.ts
+++ src/api/middleware.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
 export function log(req: Request, res: Response, next: NextFunction) {
-  console.log(`Request received: ${req.url}`);
+  console.log(`Request received: ${req.method} ${req.url}`);
   next();
 }
