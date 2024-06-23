c771385e3a4edaf6acdb04dabce36e2ef36a3faa
Cass Barnard <30302915+cass-codes@users.noreply.github.com>
2024-06-23 09:19:37 -0400

    moving game router to where it should be

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,13 +2,13 @@ import { Router } from "express";
 import { healthRouter } from "./health-router";
 import { screenRouter } from "./screen-router";
 import { saveRouter } from "./save-router";
-import { userRouter } from "./user-router";
+import { gameRouter } from "./modules/games/game-router";
 
 const apiRouter = Router();
 
 apiRouter.use("/health", healthRouter);
 apiRouter.use("/screen", screenRouter);
 apiRouter.use("/save", saveRouter);
-apiRouter.use("/user", userRouter);
+apiRouter.use("/game", gameRouter);
 
 export { apiRouter };
