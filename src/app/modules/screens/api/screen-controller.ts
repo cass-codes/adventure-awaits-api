3e88dafc0a8fd7ca6db7e4398651720108ce6f4e
Cass Barnard <30302915+cass-codes@users.noreply.github.com>
2024-06-24 22:03:25 -0400

    working route

--- src/app/modules/screens/api/screen-controller.ts
+++ src/app/modules/screens/api/screen-controller.ts
@@ -4,11 +4,11 @@ import { GetScreenDTO } from "./validation";
 
 export function getScreenById(req: GetScreenDTO, res: Response) {
   try {
-    const screenId = req.params.id;
+    const screenId = req.params.screenId;
     const gameId = req.query.gameId;
     const screen = ScreenService.getScreenById(screenId);
     const evaluatedScreen = ScreenService.evaluateScreen(screen, gameId); // Will be done in [16]
-    res.json(evaluatedScreen);
+    res.json(screen);
   } catch (error) {
     res.status(404).send(error);
   }
