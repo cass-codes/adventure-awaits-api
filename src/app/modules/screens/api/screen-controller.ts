a7ddf4ca4efd0d9e8d25fbd207ed0d859f8a0467
Cass Barnard <30302915+cass-codes@users.noreply.github.com>
2024-06-24 23:10:47 -0400

    fully working!!!!

--- src/app/modules/screens/api/screen-controller.ts
+++ src/app/modules/screens/api/screen-controller.ts
@@ -2,12 +2,12 @@ import { Response } from "express";
 import { ScreenService } from "../service/screen-service";
 import { GetScreenDTO } from "./validation";
 
-export function getScreenById(req: GetScreenDTO, res: Response) {
+export async function getScreenById(req: GetScreenDTO, res: Response) {
   try {
     const screenId = req.params.screenId;
     const { gameId, userId } = req.query;
     const screen = ScreenService.getScreenById(screenId);
-    const evaluatedScreen = ScreenService.evaluateScreen(
+    const evaluatedScreen = await ScreenService.evaluateScreen(
       screen,
       gameId,
       userId
--- src/app/modules/screens/service/types.ts
+++ src/app/modules/screens/service/types.ts
@@ -3,5 +3,5 @@ import { Game } from "../../games/service/types";
 export type ScreenFunction = (game: Game) => string | any; // TODO fix
 
 export function isScreenFunction(fn: unknown): fn is ScreenFunction {
-  return typeof fn === "function" && fn.arguments.length === 1;
+  return typeof fn === "function" && fn.length === 1;
 }
